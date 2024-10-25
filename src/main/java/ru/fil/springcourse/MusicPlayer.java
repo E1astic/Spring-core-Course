f22e6b425e4f5e8d90b02ca93f40a1e63168a0a8
Roman Filimonov <darkr2004@mail.ru>
2024-10-25 15:22:07 +0300

    annotation @qualifier (уточнение при внедрении зависимостей)

--- src/main/java/ru/fil/springcourse/MusicPlayer.java
+++ src/main/java/ru/fil/springcourse/MusicPlayer.java
@@ -1,6 +1,7 @@
 package ru.fil.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,22 +9,28 @@ import java.util.List;
 
 @Component
 public class MusicPlayer {
+    // МОЖЕМ ВНЕДРИТЬ ЗАВИСИМОСТИ ТАК - ЧЕРЕЗ ПОЛЯ,  В ЭТОМ СЛУЧАЕ ПИСАТЬ КОНСТРУКТОР НЕТ НЕОБХОДИМОСТИ
+//    @Autowired
+//    @Qualifier("classicalMusic")
+//    private Music music1;                             В ЦЕЛОМ СУЩЕСТВУЕТ 3 ВАРИАНТА ВНЕДРЕНИЯ ЗАВИСИМОСТЕЙ
+//                                                      ЧЕРЕЗ ПОЛЕ,  СЕТТЕР  ИЛИ  КОНСТРУКТОР
+//    @Autowired
+//    @Qualifier("rockMusic")
+//    private Music music2;
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
-    // пока что создали 2 поля по-отдельности, потому что при передаче в параметр единственного поля типа Music,
-    // имея при этом 2 бина ClassicalMusic и RockMusic, Spring бы не понял, какой из этих двух бинов передавать на место поля типа Music
-
+    // А МОЖЕМ И ТАК
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1=music1;
+        this.music2=music2;
     }
 
     public String playMusic(){
-        return"Playing: " + classicalMusic.getSong()+
-        "\nPlaying: " + rockMusic.getSong()+"\n";
+        return"Playing: " + music1.getSong()+",  "+music2.getSong();
     }
 
 
